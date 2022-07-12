95247a5c1034ba349787cafec72db7c05b47aba2
Kirill <62622495+exkirilg@users.noreply.github.com>
2022-07-12 17:45:24 +0300

    Move method

--- MoviesRental/Rental.cs
+++ MoviesRental/Rental.cs
@@ -13,32 +13,10 @@ public class Rental
 
     public double GetCharge()
     {
-        double result = 0;
-
-        switch (Movie.PriceCode)
-        {
-            case Movie.Regular:
-                result += 2;
-                if (DaysRented > 2)
-                    result += (DaysRented - 2) * 1.5;
-                break;
-            case Movie.NewRelease:
-                result += DaysRented * 3;
-                break;
-            case Movie.Childrens:
-                result += 1.5;
-                if (DaysRented > 3)
-                    result += (DaysRented - 3) * 1.5;
-                break;
-        }
-
-        return result;
+        return Movie.GetCharge(DaysRented);
     }
     public int GetFrequentRenterPoints()
     {
-        if (Movie.PriceCode == Movie.NewRelease && DaysRented > 1)
-            return 2;
-
-        return 1;
+        return Movie.GetFrequentRenterPoints(DaysRented);
     }
 }
