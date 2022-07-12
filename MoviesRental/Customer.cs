40d5512a7d203bc72bd553d47056ca46a2400eb1
Kirill <62622495+exkirilg@users.noreply.github.com>
2022-07-12 17:05:43 +0300

    Replace temp with query

--- MoviesRental/Customer.cs
+++ MoviesRental/Customer.cs
@@ -17,22 +17,23 @@ public class Customer
 
     public string Statement()
     {
-        double totalAmount = 0;
-        int frequentRentalPoints = 0;
-
         string result = $"Rental for {Name}\n";
 
         foreach (var aRental in Rentals)
-        {
-            frequentRentalPoints += aRental.GetFrequentRenterPoints();
-
             result += $"\t{aRental.Movie.Title}\t{aRental.GetCharge()}\n";
-            totalAmount += aRental.GetCharge();
-        }
 
-        result += $"The amount of debt is {totalAmount}\n";
-        result += $"You earned {frequentRentalPoints} for activity";
+        result += $"The amount of debt is {GetTotalCharge()}\n";
+        result += $"You earned {GetTotalFrequentRenterPoints()} for activity";
 
         return result;
+    }
+
+    private double GetTotalCharge()
+    {
+        return Rentals.Sum(r => r.GetCharge());
+    }
+    private int GetTotalFrequentRenterPoints()
+    {
+        return Rentals.Sum(r => r.GetFrequentRenterPoints());
     }
 }
